2774e4ddae01838d8157585bac5bc0d3f16d654b
Atul Verma <atulv9926@gmail.com>
2026-08-17 00:39:58 +0530

    render via html

--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello")
+    return render(request, "index.html")
+
+def about(request):
+    return HttpResponse("about page")
