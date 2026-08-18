dcc691a099a9e8765b0d9b979ac97ce7a270b751
Atul Verma <atulv9926@gmail.com>
2026-08-19 01:08:53 +0530

    model

--- djangoproject/myapp/models.py
+++ djangoproject/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    salary = models.IntegerField()
